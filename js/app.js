20c4d12bfd2958e0935a1dafd7b419d6886e3c59
AlveeGit <alveekabir1@gmail.com>
2023-01-24 22:45:36 +0600

    updated

---
+++ js/app.js
@@ -0,0 +1,41 @@
+document.getElementById('pin-btn').addEventListener('click', function () {
+    window.location.href = ("https://alveegit.github.io/pin-matcher-1/");
+}
+)
+
+document.getElementById('bank-btn').addEventListener('click', function () {
+    window.location.href = ("https://alveegit.github.io/fuctional-bank/banking.html");
+}
+)
+
+document.getElementById('panda-btn').addEventListener('click', function () {
+    window.location.href = ("https://alveegit.github.io/panda-bootstrap/");
+}
+)
+
+document.getElementById('leader-btn').addEventListener('click', function () {
+    window.location.href = ("https://alveegit.github.io/leader-board-css3/");
+}
+)
+
+document.getElementById('football-btn').addEventListener('click', function () {
+    window.location.href = ("https://alveegit.github.io/football/");
+}
+)
+document.getElementById('food-btn').addEventListener('click', function () {
+    window.location.href = ("https://alveegit.github.io/foodNetwork/");
+}
+)
+document.getElementById('resume-btn').addEventListener('click', function () {
+    window.location.href = ("https://drive.google.com/file/d/1ibQTalUqFTnJZjIwwTZfrUOxN3YwAwxw/view?usp=sharing");
+}
+)
+document.getElementById('git-btn').addEventListener('click', function () {
+    window.location.href = ("https://github.com/AlveeGit");
+}
+)
+document.getElementById('facebook-btn').addEventListener('click', function () {
+    window.location.href = ("https://www.facebook.com/MdAlveeKabir/");
+}
+)
+
